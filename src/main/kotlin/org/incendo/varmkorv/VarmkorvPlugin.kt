77472ec91884577e7f02444a9b6f2acbd7c88673
Alexander Söderberg <contact@alexander-soderberg.com>
2021-01-07 07:29:51 +0100

    Add more examples

--- src/main/kotlin/org/incendo/varmkorv/VarmkorvPlugin.kt
+++ src/main/kotlin/org/incendo/varmkorv/VarmkorvPlugin.kt
@@ -5,16 +5,24 @@ import com.google.inject.Inject
 import com.google.inject.Module
 import com.google.inject.name.Named
 import com.google.inject.name.Names
+import kotlinx.coroutines.*
+import kotlinx.coroutines.future.asDeferred
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitScheduler
 
 class VarmkorvPlugin : JavaPlugin() {
 
     @Inject
     @Named("Message")
     private lateinit var message: String
+    @Inject
+    private lateinit var scheduler: BukkitScheduler
 
-    override fun onEnable() {
-        logger.info("I'd just like to say: $message")
+    companion object {
+        /** Dispatcher that synchronizes to the primary thread */
+        lateinit var DISPATCHER: CoroutineDispatcher
     }
 
     override fun getInjectionModules(): MutableCollection<Module> {
@@ -25,4 +33,43 @@ class VarmkorvPlugin : JavaPlugin() {
         })
     }
 
+    override fun onEnable() {
+        DISPATCHER = dispatcher
+        logger.info("I'd just like to say: $message")
+        commandManager.command(
+            commandManager.commandBuilder("message")
+                .handler {
+                    it.sender.sendMessage(message)
+                }
+        ).command(
+            commandManager.commandBuilder("korvbröd")
+                .senderType(Player::class.java)
+                .handler {
+                    GlobalScope.launch {
+                        doStuff(it.sender as Player)
+                    }
+                }
+        )
+    }
+
+    /** Demonstration of random synchronization stuff */
+    private suspend fun doStuff(player: Player) {
+        withContext(Dispatchers.Default) {
+            /* Supply the chunk asynchronously and suspend until the result is available */
+            scheduler.supplySync(this@VarmkorvPlugin) {
+                player.teleport(Bukkit.getWorlds().get(1).spawnLocation)
+            }.asDeferred().await().let {
+                player.sendMessage("Teleported? ${if (it) "yes" else "no"}")
+            }
+            /* Run the block on the primary thread */
+            withContext(dispatcher) {
+                player.sendMessage("Hello dis is primary thread? ${if (Bukkit.isPrimaryThread()) "yes" else "no"}")
+            }
+        }
+    }
+
 }
+
+/* Add a dispatcher to the Dispatcher class */
+val Dispatchers.Minecraft
+    get() = VarmkorvPlugin.DISPATCHER
